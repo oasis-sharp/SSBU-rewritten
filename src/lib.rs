4dd98d8a1a1549b3c2e5b52599f333a4e0676498
Owais (Gabriel) Hussain <64927775+oasis-sharp@users.noreply.github.com>
2023-04-18 17:50:51 +0100

    fix character install

--- src/lib.rs
+++ src/lib.rs
@@ -7,22 +7,11 @@
     clippy::borrow_interior_mutable_const
 )]
 
-mod ike;
-mod ganon;
-mod mewtwo;
-mod sonic;
-mod zelda;
-mod mariod;
-mod rockman;
+mod inkling;
 
 
 #[skyline::main(name = "smashline_test")]
 pub fn main() {
-    ike::install();
-    ganon::install();
-    mewtwo::install();
-    sonic::install();
-    zelda::install();
-    mariod::install();
-    rockman::install();
+    inkling::install();
+
 }
