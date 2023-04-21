6fc9f701ef15a66c82cfbf190d4abd5b3f28d628
Owais (Gabriel) Hussain <64927775+oasis-sharp@users.noreply.github.com>
2023-04-21 23:43:06 +0100

    edits to account for new file structure

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 cargo skyline package
 pause
-move inkling_reimagined\plugin.nro other_files
+move ssbu_rewritten\plugin.nro other_files
 rename other_files\plugin.nro plugin_old.nro
 rename target\aarch64-skyline-switch\release\libplugin.nro plugin.nro
-move target\aarch64-skyline-switch\release\plugin.nro inkling_reimagined
+move target\aarch64-skyline-switch\release\plugin.nro ssbu_rewritten
