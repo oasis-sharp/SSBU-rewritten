d0c5b6a7b033ae84ae8ff0d70943fa821c492d62
Owais (Gabriel) Hussain <64927775+oasis-sharp@users.noreply.github.com>
2023-04-23 15:24:37 +0100

    build edits to add development plugin case

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 cargo skyline package
 rename target\aarch64-skyline-switch\release\libplugin.nro plugin.nro
 move target\aarch64-skyline-switch\release\plugin.nro ssbu-rewritten
+copy ssbu-rewritten\plugin.nro development-plugin
